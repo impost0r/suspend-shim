02a46caeae73b03bf92101416884cae530e99c28
impost0r <impostor@ret2p.lt>
2022-09-13 07:59:17 -0500

    Initial Commit

---
+++ suspend-shim/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  suspend-shim
+//
+//  Created by jogn on 9/13/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
