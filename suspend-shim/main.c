75cd9c313c419a9a77778aee0607d0ce4cd451c2
impost0r <impostor@ret2p.lt>
2022-09-13 08:26:26 -0500

    First commit

--- suspend-shim/main.c
+++ suspend-shim/main.c
@@ -1,14 +1,49 @@
-//
-//  main.c
-//  suspend-shim
-//
-//  Created by jogn on 9/13/22.
-//
+// suspend-shim
+// 9-13-2022 - impost0r/secret.club
+// provide application to be spawned w/o ASLR as argv[1]
+// License - GPL-3
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <spawn.h>
+#include <stdbool.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+#define POSIX_SPAWN_DISABLE_ASLR 0x0100
+#define POSIX_SPAWN_ALLOW_DATA_EXEC 0x2000 //unused?
+
+extern char **environ;
+
+bool assert_perror(int val, char* fn) {
+    if (val != 0) {
+        perror(fn);
+        return false;
+    }
+    return true;
+}
+
+int main(int argc, char* argv[]) {
+
+    pid_t pid;
+    int rv;
+    posix_spawnattr_t attrp;
+    rv = posix_spawnattr_init(&attrp);
+
+    if ( assert_perror(rv, "posix_spawnattr_init") != true ) {
+        exit(EXIT_FAILURE);
+    }
+
+    rv = posix_spawnattr_setflags(&attrp, POSIX_SPAWN_START_SUSPENDED | POSIX_SPAWN_DISABLE_ASLR); //lol
+
+    if ( assert_perror(rv, "posix_spawnattr_setflags") != true ) {
+        exit(EXIT_FAILURE);
+    }
+
+    rv = posix_spawn(&pid, argv[1], NULL, &attrp, NULL, environ);
+    if ( assert_perror(rv, "posix_spawn") != true) {
+        exit(EXIT_FAILURE);
+    }
+    printf("Spawned %s (PID: %d)", argv[1], pid);
+
+
 }
